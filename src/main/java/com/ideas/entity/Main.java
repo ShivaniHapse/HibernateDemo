cdf31cb8196723f4b76b91902d295d0211b55670
ShivaniHapse <shivani.hapse99@gmail.com>
2020-08-12 15:49:25 +0530

    Working Code

--- src/main/java/com/ideas/entity/Main.java
+++ src/main/java/com/ideas/entity/Main.java
@@ -2,10 +2,17 @@ package com.ideas.entity;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class Main {
     public static void main(String[] args) {
         Person person = new Person("firs", "last", "em@email.com");
-        Session session = SessionFactory().
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(person);
+        System.out.println(person);
+        session.getTransaction().commit();
+        session.close();
     }
 }
